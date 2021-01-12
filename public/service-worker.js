cad0951c1cd53e00bfb256fb58807a4226398856
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-01-11 19:57:18 -0500

    Heroku test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,87 +1,60 @@
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
-const FILES_TO_CACHE = [
-    // '/',
-    // '/index.html',
-    // '/assets/css/styles.css',
-    // '/assets/images/icons/icon-192x192.png',
-    // '/assets/images/icons/icon-512x512.png',
-    // '/assets/js/db.js',
-    // '/assets/js/index.js',
-    // '/dist/manifest.json',
-    // '/dist/assets/images/icons/icon_96x96.png',
-    // '/dist/assets/images/icons/icon_128x128.png',
-    // "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
-    "/",
-    "/db.js",
-    "/index.js",
-    "/manifest.json",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 
-// Install 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches
-            .open(PRECACHE)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
-    );
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// Activate
-self.addEventListener("activate", event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-            })
-            .then((cachesToDelete) => {
-                return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
-                        return caches.delete(cacheToDelete);
-                    })
-                );
-            })
-            .then(() => self.clients.claim())
-    );
-});
-
-// Run
-self.addEventListener("fetch", event => {
-    // Check if request from API was succesful
-    if (event.request.url.includes("/api/")) {
-        event.respondWith(
-            caches
-                .open(RUNTIME)
-                .then(cache => {
-                    return fetch(event.request)
-                        .then(response => {
-                            // If the API response is successful, clone data and store in the cache
-                            if (response.status === 200) {
-                                cache.put(event.request.url, response.clone());
-                            }
-                            return response;
-                        })
-                        .catch(err => {
-                            // If the network request failed, try to get it from the cache
-                            return cache.match(event.request);
-                        });
-                }).catch(err => console.log(err))
-        );
-
-        return;
-    }
-
-    // Use cache first for all other requests for performance
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
 
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
 });
