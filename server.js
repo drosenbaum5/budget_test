6d8b12b984e88bb2ea98cee85178fa6fcc3a469d
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-01-11 19:31:25 -0500

    Heroku test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
